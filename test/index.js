955f32623999234610f010dc7ef75ac575698853
Eli <ecmanrique@gmail.com>
2015-10-20 18:34:45 -0500

    fix(test): change test name

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     loadTask = require('../index.js')(gulp),
     should = require('should');
 
-describe('gulp-task-loader', function() {
+describe('gulp-load-taskload-tasksn() {
   it('load task', function () {
     gulp.loadTasks(__dirname + "/tasks");
     should.exist(gulp.tasks['b'])
